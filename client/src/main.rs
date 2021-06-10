9a8c0fbbdba55490b2bca03c50f9a0552092742b
Bogdan Pop <bogdan.pop@huawei.com>
2021-06-10 20:18:49 +0300

    feat(communication): Sending and receiving bytes

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
-}
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:34254")?;
+
+    stream.write(b"Hello Server!")?;
+    let mut buffer = [0; 20];
+    stream.read(&mut buffer)?;
+
+    println!("Received: {:#?}", buffer);
+
+    Ok(())
+} // the stream is closed here
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    println!("Connection from {}", stream.peer_addr()?);
+
+    let mut buffer = [0; 20];
+    stream.read(&mut buffer)?;
+
+    println!("Received: {:#?}", buffer);
+
+    stream.write(b"Hello Client!")?;
+
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    println!("Listening...");
+    let listener = TcpListener::bind("127.0.0.1:34254")?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?).unwrap();
+    }
+    Ok(())
 }
